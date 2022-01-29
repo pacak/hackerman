dd80c257d09188ae1470fc7c6b45a1f918301b5f
Michael Baikov <manpacket@gmail.com>
2022-01-29 09:05:38 +0800

    more wip

--- src/feat_graph.rs
+++ src/feat_graph.rs
@@ -1,10 +1,8 @@
-use std::{
-    ascii::AsciiExt,
-    borrow::Cow,
-    collections::{BTreeMap, BTreeSet},
-};
+use std::borrow::Cow;
+use std::collections::{BTreeMap, BTreeSet};
+use std::ops::Index;
 
-use cargo_metadata::{DepKindInfo, Dependency, Metadata};
+use cargo_metadata::{DepKindInfo, Dependency, Metadata, PackageId};
 use dot::{GraphWalk, Labeller};
 use guppy::graph::{
     feature::{FeatureGraph, FeatureId},
@@ -115,9 +113,25 @@ pub struct FeatGraph2<'a> {
     pub features: Graph<Feature<'a>, Link<'a>>,
     pub fids: BTreeMap<Fid<'a>, NodeIndex>,
     pub pids: BTreeMap<Pid<'a>, NodeIndex>,
+    pub cache: BTreeMap<&'a PackageId, Pid<'a>>,
 }
 
-fn find_dep_by_name<'a>(name: &'a str, deps: &'a [Dependency]) -> anyhow::Result<&'a Dependency> {
+fn find_dep_by_pid<'a>(deps: &'a [Dependency], pid: Pid<'a>) -> anyhow::Result<&'a Dependency> {
+    let name = &pid.package().name;
+    // there are some very strange ideas about what is a valid crate is name and how to compare
+    // them out there
+    fn cmp(a: &str, b: &str) -> bool {
+        a.chars().zip(b.chars()).all(|(l, r)| {
+            l.to_ascii_lowercase() == r.to_ascii_lowercase() || (l == '-' && r == '_')
+        })
+    }
+
+    deps.iter()
+        .find(|d| cmp(&d.name, name) || d.rename.as_ref().map_or(false, |r| r == name))
+        .ok_or_else(|| anyhow::anyhow!("No dependency named {name}"))
+}
+
+fn find_dep_by_name<'a>(deps: &'a [Dependency], name: &'a str) -> anyhow::Result<&'a Dependency> {
     // there are some very strange ideas about what is a valid crate is name and how to compare
     // them out there
     fn cmp(a: &str, b: &str) -> bool {
@@ -143,28 +157,39 @@ impl<'a> FeatGraph2<'a> {
     }
 
     pub fn init(meta: &'a Metadata) -> anyhow::Result<Self> {
-        let workspace_members = BTreeSet::new();
-
         let resolves = &meta
             .resolve
             .as_ref()
             .ok_or_else(|| anyhow::anyhow!("Couldn't resolve the depdendencies"))?
             .nodes;
 
+        let cache = meta
+            .packages
+            .iter()
+            .enumerate()
+            .map(|(ix, package)| (&package.id, Pid(ix, meta)))
+            .collect::<BTreeMap<_, _>>();
+
         let mut f = FeatGraph2 {
-            workspace_members,
+            workspace_members: meta
+                .workspace_members
+                .iter()
+                .flat_map(|pid| cache.get(pid))
+                .copied()
+                .collect::<BTreeSet<_>>(),
             features: Graph::new(),
             fids: BTreeMap::new(),
             pids: BTreeMap::new(),
+            cache,
         };
 
         for (ix, (package, deps)) in meta.packages.iter().zip(resolves.iter()).enumerate() {
             assert_eq!(package.id, deps.id);
-            //            links.clear();
 
             println!("package: {:?}", package);
             println!("resolves: {:?}", deps);
 
+            /*
             for dep in package.dependencies.iter() {
                 println!(
                     "\t{:?}: opt: {:?}, def: {:?} {:?}",
@@ -174,25 +199,16 @@ impl<'a> FeatGraph2<'a> {
 
             for dep in deps.deps.iter() {
                 println!("\t,{:?}", dep);
-            }
+            }*/
 
-            let in_workspace = meta.workspace_members.contains(&package.id);
             let pid = Pid(ix, meta);
             let fid = Fid(pid, None);
 
-            if in_workspace {
-                f.workspace_members.insert(pid);
-            }
-
+            // handle dependencies for other packages
             for dep in deps.deps.iter() {
-                let dep_ix = meta.packages.iter().position(|p| p.id == dep.pkg).unwrap();
-                let dep_pid = Pid(dep_ix, meta);
+                let dep_pid = *f.cache.get(&dep.pkg).unwrap();
 
-                let dep_declaration =
-                    find_dep_by_name(&dep.name, &package.dependencies).or_else(|_| {
-                        let actual_name = &meta[&dep.pkg].name;
-                        find_dep_by_name(actual_name, &package.dependencies)
-                    })?;
+                let dep_declaration = find_dep_by_pid(&package.dependencies, dep_pid)?;
                 let link = Link {
                     optional: dep_declaration.optional,
                     kinds: &dep.dep_kinds,
@@ -203,69 +219,78 @@ impl<'a> FeatGraph2<'a> {
                     f.fid_index(Fid(pid, None))
                 };
 
-                if dep_declaration.features.is_empty() {
-                    let to_ix = f.fid_index(Fid(dep_pid, None));
-                    f.features.add_edge(link_source, to_ix, link);
-                } else {
-                    for feat in dep_declaration.features.iter() {
-                        let to_ix = f.fid_index(Fid(dep_pid, Some(feat)));
-                        f.features.add_edge(link_source, to_ix, link);
-                    }
-                };
-            }
-            if package.name != "web-sys" && package.name != "winapi" {
-                for (local_feat, local_deps) in package.features.iter() {
-                    let local_ix = f.fid_index(Fid(pid, Some(local_feat)));
-                    for other in local_deps.iter() {
-                        match other.split_once('/') {
-                            Some((a, b)) => {} // todo!("{:?} {:?}", a, b),
-                            None => {
-                                let other_ix = f.fid_index(Fid(pid, Some(other)));
-                                f.features.add_edge(
-                                    local_ix,
-                                    other_ix,
-                                    Link {
-                                        optional: false,
-                                        kinds: &[],
-                                    },
-                                );
-                            }
-                        }
-                    }
-                    //                println!("{local_feat:?}\n{local_deps:?}")
+                let base_dep_ix = f.fid_index(Fid(dep_pid, None));
+                f.features.add_edge(link_source, base_dep_ix, link);
+
+                for feat in dep_declaration.features.iter() {
+                    let feat_dep_ix = f.fid_index(Fid(dep_pid, Some(feat)));
+                    f.features.add_edge(link_source, feat_dep_ix, link);
                 }
             }
 
-            let feats = std::iter::once(Fid(pid, None))
-                .chain(
-                    package
-                        .dependencies
-                        .iter()
-                        .filter(|d| d.optional)
-                        .map(|d| Fid(pid, Some(&d.name))),
-                )
-                .chain(package.features.keys().map(|n| Fid(pid, Some(n))));
+            let base_ix = f.fid_index(fid);
+            for (local_feat, local_deps) in package.features.iter() {
+                let local_ix = f.fid_index(Fid(pid, Some(local_feat)));
 
-            println!("pid: {:?}", pid);
-
-            //            for l in links.iter() {
-            //                println!("\t{:?}", l);
-            //            }
-
-            for fid in feats {
-                println!("\tfid: {:?}", fid);
+                let local_link = Link {
+                    optional: false,
+                    kinds: &[],
+                };
+                f.features.add_edge(local_ix, base_ix, local_link);
+                /*
+                                for other in local_deps.iter() {
+                                    match other.split_once('/') {
+                                        Some((other_name, other_feat)) => {
+                                            let dep_declaration = find_dep_by_name(&package.dependencies, other_name)?;
+                                            let dep_pkg = deps.deps.iter().find(|d| d.name == dep_declaration.name).unwrap().pkg;
+                                            let dep_pid = f.cache.get(&dep_pkg).unwrap();
+                                            let link = Link {
+                                                optional: dep_declaration.optional,
+                                                kinds: &dep_declaration.dep_kinds,
+                                            };
+                                            let other_ix = f.fid_index(dep_pid)
+                                        } // todo!("{:?} {:?}", a, b),
+                                        None => {
+                                            let other_ix = f.fid_index(Fid(pid, Some(other)));
+                                            f.features.add_edge(local_ix, other_ix, local_link);
+                                        }
+                                    }
+                                }
+                */
+                //                println!("{local_feat:?}\n{local_deps:?}")
             }
-            for dep in package.dependencies.iter() {
-                println!(
-                    "\t{:?}: opt: {:?}, def: {:?} {:?}",
-                    dep.kind, dep.optional, dep.uses_default_features, dep
-                );
-            }
 
-            for dep in deps.deps.iter() {
-                println!("\t,{:?}", dep);
-            }
+            /*
+                        let feats = std::iter::once(Fid(pid, None))
+                            .chain(
+                                package
+                                    .dependencies
+                                    .iter()
+                                    .filter(|d| d.optional)
+                                    .map(|d| Fid(pid, Some(&d.name))),
+                            )
+                            .chain(package.features.keys().map(|n| Fid(pid, Some(n))));
 
+                        println!("pid: {:?}", pid);
+
+                        //            for l in links.iter() {
+                        //                println!("\t{:?}", l);
+                        //            }
+
+                        for fid in feats {
+                            println!("\tfid: {:?}", fid);
+                        }
+                        for dep in package.dependencies.iter() {
+                            println!(
+                                "\t{:?}: opt: {:?}, def: {:?} {:?}",
+                                dep.kind, dep.optional, dep.uses_default_features, dep
+                            );
+                        }
+
+                        for dep in deps.deps.iter() {
+                            println!("\t,{:?}", dep);
+                        }
+            */
             println!("\n\n");
         }
 
@@ -275,6 +300,12 @@ impl<'a> FeatGraph2<'a> {
 
 #[derive(Copy, Clone)]
 pub struct Pid<'a>(usize, &'a Metadata);
+
+impl Pid<'_> {
+    pub fn package(&self) -> &cargo_metadata::Package {
+        &self.1.packages[self.0]
+    }
+}
 
 impl<'a> PartialEq for Pid<'a> {
     fn eq(&self, other: &Self) -> bool {
@@ -364,8 +395,16 @@ impl<'a> Labeller<'a, NodeIndex, EdgeIndex> for &FeatGraph2<'a> {
         dot::LabelText::LabelStr("".into())
     }
 
-    fn node_style(&'a self, _n: &NodeIndex) -> dot::Style {
-        dot::Style::None
+    fn node_style(&'a self, n: &NodeIndex) -> dot::Style {
+        if let Some(fid) = self.features[*n].fid() {
+            if self.workspace_members.contains(&fid.0) {
+                dot::Style::None
+            } else {
+                dot::Style::Filled
+            }
+        } else {
+            dot::Style::None
+        }
     }
 
     fn node_color(&'a self, _node: &NodeIndex) -> Option<dot::LabelText<'a>> {
@@ -384,8 +423,12 @@ impl<'a> Labeller<'a, NodeIndex, EdgeIndex> for &FeatGraph2<'a> {
         dot::Style::None
     }
 
-    fn edge_color(&'a self, _e: &EdgeIndex) -> Option<dot::LabelText<'a>> {
-        None
+    fn edge_color(&'a self, e: &EdgeIndex) -> Option<dot::LabelText<'a>> {
+        if self.features[*e].optional {
+            Some(dot::LabelText::label("grey"))
+        } else {
+            Some(dot::LabelText::label("black"))
+        }
     }
 
     fn kind(&self) -> dot::Kind {
